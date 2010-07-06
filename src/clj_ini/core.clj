45175ae6302e4605b45adc2f38a17a0b0aa1fb38
MayDaniel <yamnad1@gmail.com>
2010-07-06 23:10:57 +0100

    What was I thinking?

--- src/clj_ini/core.clj
+++ src/clj_ini/core.clj
@@ -1,8 +1,7 @@
 (ns clj-ini.core
   (:use [clojure.contrib.str-utils :only [re-split]]
         [clojure.contrib.duck-streams :only [read-lines append-spit spit]]
-        [clojure.contrib.seq-utils :only [includes?]]
-        [clojure.contrib.core :only [seqable?]])
+        [clojure.contrib.seq-utils :only [includes?]])
   (:import [java.io File]))
 
 (defn check-create
@@ -20,7 +19,7 @@
   (letfn [(extract-comments [src] (take-while #(= (first %) \#) src))]
     (with-meta {}
       (cond (string? source) {:comments (extract-comments (read-lines (check-create source)))}
-            (and (seqable? source) (every? string? source)) {:comments (extract-comments source)}
+            (and (coll? source) (every? string? source)) {:comments (extract-comments source)}
             :else (throw (Exception. "Source should be either a sequence of strings, or a file-name."))))))
 
 (defn read-map
@@ -32,7 +31,7 @@ if it does not exist. Any comment metadata will be included."
         meta-comments (meta (get-comments contents))]
     (if (empty? data-lines) {}
         (loop [lines data-lines acc {}]
-          (if (empty? lines) (with-meta acc meta-comments)
+          (if lines (with-meta acc meta-comments)
               (let [[x & more] lines
                     [key val] (re-split #" = " x 2)]
                 (recur more (assoc acc (read-string key) (read-string val)))))))))
@@ -48,7 +47,7 @@ metadata to include in the dump."
         (append-spit file (str "# " line \newline)))
       (append-spit file \newline))
     (loop [out-map (merge contents map)]
-      (when-not (empty? out-map)
+      (when out-map
         (let [kv (first out-map)]
           (append-spit file (str (key kv) " = " (val kv) \newline)))
-        (recur (rest out-map))))))
+        (recur (next out-map))))))
