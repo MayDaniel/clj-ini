f5a82931ad5dc5fcd5ce42fa084f8e32b86383ed
MayDaniel <yamnad1@gmail.com>
2010-08-28 19:26:36 +0100

    Updated to 1.2.0

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject clj-ini "0.1.1"
+(defproject clj-ini "0.1.2"
   :description "Another small, superficial read/write key/val configuration utility."
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-clojars "0.5.0"]]
-  :namespaces [clj-ini.core])
+                     [lein-clojars "0.5.0"]])
